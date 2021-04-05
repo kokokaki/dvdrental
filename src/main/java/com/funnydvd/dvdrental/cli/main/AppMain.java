6da13056a90970da9d5530ad28236e8cf6dc28f7
kokokaki <hsg9984@kitt.co.kr>
2021-04-05 15:35:57 +0900

    [21.04.05] # movie 관리 시스템 완성.

---
+++ src/main/java/com/funnydvd/dvdrental/cli/main/AppMain.java
@@ -0,0 +1,20 @@
+package com.funnydvd.dvdrental.cli.main;
+
+import com.funnydvd.dvdrental.cli.ui.AppUI;
+
+import static com.funnydvd.dvdrental.cli.ui.AppUI.*;
+
+public class AppMain {
+
+    public static void main(String[] args) {
+
+        while (true) {
+
+            startScreen();
+            int selection = inputInteger(">>> ");
+
+            FrontController.chooseSystem(selection);
+
+        }
+    }
+}
